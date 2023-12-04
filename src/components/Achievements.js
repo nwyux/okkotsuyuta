e117ea0ca00808ea83b615be9f482a4107ad7f5b
noah <101118381+nwyux@users.noreply.github.com>
2023-12-04 10:44:28 +0100

    achievements

--- src/components/Achievements.js
+++ src/components/Achievements.js
@@ -1,18 +1,43 @@
+import achievements from "./Data";
+
 export default function Achievements() {
   return (
-    <div id="achievements" className="text-polaire flex flex-col justify-center">
+    <div
+      id="achievements"
+      className="text-polaire flex flex-col justify-center"
+    >
       <div className="flex flex-col justify-center items-center gap-4 py-4">
         <h1 className="text-4xl font-jjk">My achievements</h1>
       </div>
 
       <div className="flex flex-col mx-auto justify-center items-center gap-4 py-4 ">
-
-        <div className="flex flex-col justify-center items-center gap-4 py-4 border rounded border-polaire ">
-          <h1 className="text-2xl font-jjk">Sorcerer</h1>
-          <p className="text-sm font-bold">Special Grade</p>
-          <img src="https://i.imgur.com/5ZQZ3ZB.png" alt="sorcerer" className="w-full"/>
-        </div>
-
+        {achievements.map((achievement) => {
+          const { id, title, description, img, alt, category } = achievement;
+          return (
+            <>
+              <div data-aos="fade-up" data-aos-duration="700" key={id} className="projectCard flex flex-col justify-center items-center gap-4 py-4 border border-zinc-400 rounded-lg max-w-xl">
+                <div className="flex flex-col sm:flex-row justify-center items-center gap-4 px-4">
+                  <div className="flex flex-col justify-center items-center gap-4 py-2">
+                    <h1 className="text-2xl font-jjk">{title}</h1>
+                    <img
+                      className="max-w-xs border border-zinc-400 rounded"
+                      src={img}
+                      alt={alt}
+                    />
+                  </div>
+                  <div className="flex flex-col justify-between items-center gap-2">
+                  <p className="text-sm font-bold items-center break-all">
+                    {description}
+                  </p>
+                  <p className="text-sm font-bold items-center break-all py-2 px-2 border border-moon-200  bg-moon-600">
+                    {category}
+                  </p>
+                  </div>
+                </div>
+              </div>
+            </>
+          );
+        })}
       </div>
     </div>
   );
---
+++ src/components/Data.js
@@ -0,0 +1,22 @@
+import vsgeto from '../img/vsgeto.webp'
+
+const achievements = [
+    {
+        id: 1,
+        title: 'Fight against Suguru Geto',
+        description: 'Durang an event in the Jujutsu School, Geto sneakily wen through the school protection barrier and robbed cursed objects. I catched him and fought him on my own. I won.',
+        img: vsgeto,
+        alt: 'Okkotsu Yuta vs Suguru Geto',
+        category: 'Fight',
+    },
+    {
+        id: 2,
+        title: 'Všetko je možné',
+        description: 'Všetko je možné, len treba chcieť a urobiť pre to všetko.',
+        img: vsgeto,
+        alt: 'Všetko je možné',
+        category: 'Rescue mission',
+    },
+]
+
+export default achievements;
